cb209cce9be618a2bd443cf102ec6932b6ff4aef
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2023-06-30 17:47:47 +0530

    Uploaded Day-06

---
+++ Day-04/Day-06/P-02(Left Shift Operator).cpp
@@ -0,0 +1,19 @@
+// WAP for left shift operator.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a , n;
+
+    cout << "Enter value of A -: ";
+    cin >> a;
+
+    cout << "Enter number of times you want to left shift -: ";
+    cin >> n;
+
+    cout << "Left shift is -: " << ( a << n ) << endl;
+
+}
---
+++ Day-04/Day-06/P-04.cpp
@@ -0,0 +1,22 @@
+// WAP to print counting of numbers.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+
+    cout << "Enter number upto which you want to print counting -: ";
+    cin >> n;
+
+    for ( int i = 0; i < n; i++)
+    {
+        
+        cout << i << " " << endl;
+
+    }
+    
+
+}
