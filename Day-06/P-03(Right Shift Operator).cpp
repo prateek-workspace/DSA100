1bfe1c5ceb99ad2d633a209743b65f770aa27598
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2023-07-01 21:50:10 +0530

    Uploaded Day-06

---
+++ Day-06/P-03(Right Shift Operator).cpp
@@ -0,0 +1,19 @@
+// WAP for right shift operator.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a , n;
+
+    cout << "Enter value of A -: ";
+    cin >> a;
+
+    cout << "Enter number of times you want to right shift -: ";
+    cin >> n;
+
+    cout << "right shift is -: " << ( a >> n ) << endl;
+
+}
